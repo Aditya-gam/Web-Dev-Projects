5de6644ed49ddfd768501d3c590c07866c49fef2
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2022-11-08 11:07:00 +0530

    Added new scenes
    Added Bar, line, pie and geography charts. Added event calendar and faq page. added new isSidebar state.

--- Admin Dashboard/admin-dashboard/src/App.js
+++ Admin Dashboard/admin-dashboard/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { Routes, Route } from "react-router-dom";
@@ -10,37 +10,38 @@ import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
 import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
-// import Bar from "./scenes/bar";
+import Bar from "./scenes/bar";
 import Form from "./scenes/form";
-// import Line from "./scenes/line";
-// import Pie from "./scenes/pie";
-// import FAQ from "./scenes/faq";
-// import Geography from "./scenes/geography";
-// import Calendar from "./scenes/calendar";
+import Line from "./scenes/line";
+import Pie from "./scenes/pie";
+import FAQ from "./scenes/faq";
+import Geography from "./scenes/geography";
+import Calendar from "./scenes/calendar";
 
 function App() {
   const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
 
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          <Sidebar />
+          <Sidebar isSidebar={isSidebar} />
           <main className="content">
-            <Topbar />
+            <Topbar setIsSidebar={setIsSidebar} />
             <Routes>
               <Route path="/" element={<Dashboard />}></Route>
               <Route path="/team" element={<Team />}></Route>
               <Route path="/invoices" element={<Invoices />}></Route>
               <Route path="/contacts" element={<Contacts />}></Route>
-              {/* <Route path="/bar" element={<Bar />}></Route> */}
+              <Route path="/bar" element={<Bar />}></Route>
               <Route path="/form" element={<Form />}></Route>
-              {/* <Route path="/line" element={<Line />}></Route> */}
-              {/* <Route path="/pie" element={<Pie />}></Route> */}
-              {/* <Route path="/faq" element={<FAQ />}></Route> */}
-              {/* <Route path="/geography" element={<Geography />}></Route> */}
-              {/* <Route path="/calendar" element={<Calendar />}></Route> */}
+              <Route path="/line" element={<Line />}></Route>
+              <Route path="/pie" element={<Pie />}></Route>
+              <Route path="/faq" element={<FAQ />}></Route>
+              <Route path="/geography" element={<Geography />}></Route>
+              <Route path="/calendar" element={<Calendar />}></Route>
             </Routes>
           </main>
         </div>
